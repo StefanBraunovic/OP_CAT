ee850fba6e86662322deb4dd1001a989ccd5d210
Stefan Braunovic <stefan.braunovic@vegait.rs>
2024-01-18 17:44:34 +0100

    animating cat with og code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom
 import Home  from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar';
 import EatingCat from './pages/EatingCat/EatingCat';
+import WalkingCat from './pages/WalkingCat/WalkingCat';
 function App() {
   return (
     <Router>
@@ -11,7 +12,7 @@ function App() {
       <Navbar />
       <Routes>
       <Route path="/OP" element={<Home />} />
-      <Route path="/CAT" element={<Home />} />
+      <Route path="/CAT" element={<WalkingCat />} />
       <Route path="/OP_CAT" element={<Home />} />
       <Route path="/FEED_CAT" element={<EatingCat />} />
 
