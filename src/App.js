c443ac38476872d2d5ba40383f65cd72e757176a
shnifty <shnifty.dev@gmail.com>
2024-01-19 16:49:38 +1100

    Add OPCAT page and implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import Home  from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar';
+import OPCAT from './pages/OPCAT/OPCAT';
 function App() {
   return (
     <Router>
@@ -11,8 +12,8 @@ function App() {
       <Routes>
       <Route path="/OP" element={<Home />} />
       <Route path="/CAT" element={<Home />} />
-      <Route path="/OP_CAT" element={<Home />} />
-        <Route path="/" element={<Home />} />
+      <Route path="/OP_CAT" element={<OPCAT />} />
+      <Route path="/" element={<Home />} />
       </Routes>
     </div>
   </Router>
---
+++ src/components/OPCAT/OpCatComponent.js
@@ -0,0 +1,110 @@
+// CatComponent.js
+import React from 'react';
+import styles from './OpCatComponent.module.css'
+
+const OpCatComponent = () => {
+  return (
+    <>
+    {/* Step 1*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step1.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 1: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    {/* Step 2*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step2.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 2: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    {/* Step 3*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step3.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 3: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    {/* Step 4*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step4.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 4: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    {/* Step 5*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step5.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 5: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    {/* Step 6*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step6.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 6: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    {/* Step 7*/}
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <img
+          src={process.env.PUBLIC_URL + '/step7.gif'} // Replace with the path to your animated GIF
+          alt="Animated GIF"
+          className={styles.imgres} />
+      </div>
+      <div className={styles.column}>
+        <h1 className={styles.header}>Step 7: Header Name Here</h1>
+        <p className={styles.text}>
+          Claw drapes give attitude hopped up on goofballs missing until dinner time nap all day run in circles stand in front of the computer screen stretch why must they do that. All of a sudden go crazy cat snacks chew ipad power cord climb leg find something else more interesting hate dog hide when guests come over hopped up on goofballs inspect anything brought into the house intently sniff hand leave dead animals as gifts make muffins mark territory missing until dinner time need to chase tail or need to chase tail run in circles stretch swat at dog throwup on your pillow under the bed use lap as chair why must they do that.      </p>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default OpCatComponent;
