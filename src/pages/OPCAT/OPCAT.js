ead50b2e9edb1ddb707c42246efa528a1b4cfa98
shnifty <shnifty.dev@gmail.com>
2024-01-22 12:33:53 +1100

    Updates on styling for OP_CAT page

--- src/pages/OPCAT/OPCAT.js
+++ src/pages/OPCAT/OPCAT.js
@@ -6,6 +6,7 @@ import OpCatComponent from '../../components/OPCAT/OpCatComponent';
 const OPCAT = () => {
   return (
     <div className={styles.container}>
+      <h2 className={styles.header}>OP_CAT In Action</h2>
       <OpCatComponent />
     </div>
   );
